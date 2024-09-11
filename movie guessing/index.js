08d3af42e75863f6262edc1f0ddcf5fbcd1a000c
Anshu Priya <anshupriyadnr06@gmail.com>
2024-09-11 17:53:57 +0530

    new update

--- movie guessing/index.js
+++ movie guessing/index.js
@@ -2,3 +2,16 @@
 let a = prompt("enter name")
 console.log(a)
 alert("this is my website .... don't try to hack it")
+
+const favMovie = 'Stree';
+let guess = prompt('Guess my favourite movie.');
+
+while((guess!= favMovie) && (guess!= 'quit')){
+    guess = prompt("Wrong guess. Please try again");
+}
+
+if(guess == favMovie){
+    console.log("congrats!!");
+}else{
+    console.log("you quit!!");   
+}
